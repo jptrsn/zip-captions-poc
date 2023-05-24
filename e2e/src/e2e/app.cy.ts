face8d76879a66aa58aa12669948f4cf487e603e
James <ptrsnja@gmail.com>
2023-05-24 16:19:33 -0400

    Initial commit

---
+++ e2e/src/e2e/app.cy.ts
@@ -0,0 +1,13 @@
+import { getGreeting } from '../support/app.po';
+
+describe('zip-captions', () => {
+  beforeEach(() => cy.visit('/'));
+
+  it('should display welcome message', () => {
+    // Custom command example, see `../support/commands.ts` file
+    cy.login('my-email@something.com', 'myPassword');
+
+    // Function helper example, see `../support/app.po.ts` file
+    getGreeting().contains('Welcome zip-captions');
+  });
+});
